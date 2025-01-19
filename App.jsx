1991ef3d49a2d4edba3743acb8d1902bbc52c0bc
Vasya Humen <166016202+HumenVAsya@users.noreply.github.com>
2025-01-19 15:37:47 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/icon.png";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App;
